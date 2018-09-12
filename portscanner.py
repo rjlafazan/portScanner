39a894b3b5a2234a70545a10ab76e86b4a3d8e68
Ryan <rjlafazan@gmail.com>
2018-09-12 11:26:05 -0700

    program initiate

--- portscanner.py
+++ portscanner.py
@@ -7,6 +7,14 @@ host = ''
 max_port = 5000
 min_port = 1
 
+#initiating program
+try:
+    host = raw_input("[*] Enter Target Host Address: ")
+except KeyboardInterrupt: 
+    print("\n\n[*] User Requested An Interrupt.")
+    print("[*] Application Shutting Down.")
+    sys.exit(1)
 
+hostip = gethostbyname(host)
 
 
